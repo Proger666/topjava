21273c378dbf55fa753c47492aa10277ad35236d
Proger666 <knorra@mail.ru>
2016-04-23 11:01:39 +0300

    hw05 ready to do

---
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserMealRepositoryImpl.java
@@ -0,0 +1,45 @@
+package ru.javawebinar.topjava.repository.datajpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.repository.UserMealRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * GKislin
+ * 27.03.2015.
+ */
+@Repository
+public class DataJpaUserMealRepositoryImpl implements UserMealRepository{
+
+    @Autowired
+    ProxyMealRepository proxy;
+
+    @Override
+    public UserMeal save(UserMeal userMeal, int userId) {
+
+    }
+
+    @Override
+    public boolean delete(int id, int userId) {
+        return false;
+    }
+
+    @Override
+    public UserMeal get(int id, int userId) {
+        return null;
+    }
+
+    @Override
+    public List<UserMeal> getAll(int userId) {
+        return null;
+    }
+
+    @Override
+    public List<UserMeal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
+        return null;
+    }
+}
